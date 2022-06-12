74eccbb9e706af6ed245e56aa5c8a2a737bae48c
Roger Ruiz <roger.ruiz.21@gmail.com>
2022-06-12 08:31:16 -0400

    first commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() throws Exception {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://captive.apple.com/");
+
+        driver.manage().window().maximize();
+
+        WebElement successMessage = driver.findElement(By.xpath("//body[contains(text(), 'Success')]"));
+
+        Assert.assertEquals(successMessage, "success", "Something happened !");
+    }
+}
